3e82bd1331f51ca68a2620a16f813305e7e1a6ca
unknown <you@example.com>
2019-02-24 23:40:06 -0300

    continuacao curso modulo 7

--- public/js/directives/minhas-diretivas.js
+++ public/js/directives/minhas-diretivas.js
@@ -1,1 +1,22 @@
-angular.module('alurapic', []);
+angular.module('minhasDiretivas', []).directive('meuPainel', function() {
+
+        var ddo = {};
+
+        ddo.restrict = "AE";
+        ddo.transclude = true;
+
+        ddo.scope = {
+            titulo: '@'
+        };
+
+        ddo.template = 
+                '<div class="panel panel-default">'
+            +   '   <div class="panel-heading">'
+            +   '        <h3 class="panel-title text-center">{{titulo}}</h3> '
+            +   '   </div>'
+            +   '   <div class="panel-body" ng-transclude>'
+            +   '   </div>'
+            +   '</div>'
+
+        return ddo;
+    });
